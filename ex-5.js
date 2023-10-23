720b5743de024d970bac15eaab8bc97da69520e7
Kawin <kawind7@gmail.com>
2023-10-23 21:20:32 +0700

    ES6 #1-7

--- ex-5.js
+++ ex-5.js
@@ -1,6 +1,8 @@
-function sum() {
+function sum(...input) {
   // Start coding here !
+  return input.reduce((accumulator, current) => accumulator + current, 0);
 }
+
 
 let result1 = sum(1, 2, 3, 4, 5, 6, 7, 8);
 let result2 = sum(1, 2, 3, 4);
